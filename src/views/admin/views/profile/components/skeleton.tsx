3c85d8840fbd9b8c54157b1645769d79693805f0
acerohernan <contacto.acero.hernan@gmail.com>
2022-12-14 09:55:09 -0500

    build: adding loader to page loading time

--- src/views/admin/views/profile/components/skeleton.tsx
+++ src/views/admin/views/profile/components/skeleton.tsx
@@ -1,14 +1,24 @@
 const ProfilePageSkeleton = () => {
   return (
     <div className="w-full grid gap-4 mt-4 lg:mt-14">
-      <div className="bg-white card w-full h-[330px]" />
-      <div className="bg-white card w-full h-[280px]" />
+      <div className="bg-white card w-full h-[330px] flex items-center justify-center">
+        <div className="dot-pulse" />
+      </div>
+      <div className="bg-white card w-full h-[280px] flex items-center justify-center">
+        <div className="dot-pulse" />
+      </div>
       <div className="grid gap-4 lg:grid-cols-2">
         <div className="grid gap-4">
-          <div className="card h-[188px]" />
-          <div className="card h-[106px]" />
+          <div className="card flex items-center justify-center h-[188px]">
+            <div className="dot-pulse" />
+          </div>
+          <div className="card flex items-center justify-center h-[106px]">
+            <div className="dot-pulse" />
+          </div>
         </div>
-        <div className="card h-[226px] lg:h-full" />
+        <div className="card flex items-center justify-center h-[226px] lg:h-full">
+          <div className="dot-pulse" />
+        </div>
       </div>
     </div>
   );
